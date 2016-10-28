9a5de4843aee8c946b35d1cde5383de0727bd1fb
Alex Dadgar <alex.dadgar@gmail.com>
2016-10-28 10:13:25 -0700

    Watch job status

--- command/job_watch.go
+++ command/job_watch.go
@@ -3,6 +3,7 @@ package command
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	ui "github.com/gizak/termui"
 	"github.com/hashicorp/nomad/api"
@@ -12,6 +13,12 @@ type JobWatchCommand struct {
 	Meta
 	config *JobWatchConfig
 	client *api.Client
+
+	// err holds an error that occured that should be displayed to the user
+	err error
+
+	// jobStopped marks a job as being stopped
+	jobStopped bool
 }
 
 type JobWatchConfig struct {
@@ -72,50 +79,74 @@ func (c *JobWatchCommand) Run(args []string) int {
 	}
 	c.client = client
 
-	return c.run()
+	// Try querying the job
+	jobID := c.config.JobID
+	jobs, _, err := c.client.Jobs().PrefixList(jobID)
+	if err != nil {
+		c.Ui.Error(fmt.Sprintf("Error querying job: %s", err))
+		return 1
+	}
+	if len(jobs) == 0 {
+		c.Ui.Error(fmt.Sprintf("No job(s) with prefix or id %q found", jobID))
+		return 1
+	}
+	if len(jobs) > 1 && strings.TrimSpace(jobID) != jobs[0].ID {
+		c.Ui.Output(fmt.Sprintf("Prefix matched multiple jobs\n\n%s", createStatusListOutput(jobs)))
+		return 0
+	}
+	// Prefix lookup matched a single job
+	job, _, err := c.client.Jobs().Info(jobs[0].ID, nil)
+	if err != nil {
+		c.Ui.Error(fmt.Sprintf("Error querying job: %s", err))
+		return 1
+	}
+
+	return c.run(job)
 }
 
-func (c *JobWatchCommand) run() int {
+func (c *JobWatchCommand) run(job *api.Job) int {
 	err := ui.Init()
 	if err != nil {
 		c.Ui.Error(fmt.Sprintf("Initializing UI failed: %v", err))
 		return 1
 	}
-	defer ui.Close()
+	defer func() {
+		// Clear and close
+		ui.Close()
+
+		if c.err != nil {
+			c.Ui.Error(fmt.Sprintf("Exiting on error: %v", err))
+		} else if c.jobStopped {
+			c.Ui.Output(fmt.Sprintf("Job %q was stopped", job.ID))
+		}
+	}()
+
+	// Start the watchers
+	go c.pollJob(job)
 
 	// Add the exit gauge
 	if c.config.ExitAfter != 0 {
 		c.addExitAfterGauge()
 	}
 
-	ls := ui.NewList()
-	ls.Border = false
-	ls.Items = []string{
-		"[1] Downloading File 1",
-		"", // == \newline
-		"[2] Downloading File 2",
-		"",
-		"[3] Uploading File 3",
-	}
-	ls.Height = 5
-
-	par := ui.NewPar("<> This row has 3 columns\n<- Widgets can be stacked up like left side\n<- Stacked widgets are treated as a single widget")
-	par.Height = 5
-	par.BorderLabel = "Demonstration"
+	// Get the job status
+	status := NewJobStatusGrid(job)
 
 	// build layout
 	ui.Body.AddRows(
-		ui.NewRow(
-			ui.NewCol(3, 0, ls),
-			ui.NewCol(6, 0, par)))
+		ui.NewRow(ui.NewCol(6, 0, status)),
+	)
 
 	// calculate layout
 	ui.Body.Align()
 
 	ui.Render(ui.Body)
 
-	ui.Handle("/sys/kbd/q", func(ui.Event) {
+	ui.Handle("/sys/kbd", func(ui.Event) {
 		ui.StopLoop()
+	})
+
+	ui.Handle("/sys/kbd/c", func(ui.Event) {
 	})
 
 	ui.Handle("/sys/wnd/resize", func(e ui.Event) {
@@ -125,7 +156,27 @@ func (c *JobWatchCommand) run() int {
 		ui.Render(ui.Body)
 	})
 
+	ui.Handle("/error", func(e ui.Event) {
+		err, ok := e.Data.(error)
+		if !ok {
+			panic(e.Data)
+		}
+
+		c.err = err
+		ui.StopLoop()
+	})
+
+	ui.Handle("/job/stopped", func(ui.Event) {
+		c.jobStopped = true
+		ui.StopLoop()
+	})
+
 	ui.Loop()
+
+	if c.err != nil {
+		return 1
+	}
+
 	return 0
 }
 
@@ -153,7 +204,9 @@ func (c *JobWatchCommand) addExitAfterGauge() {
 	exitGauge.Handle("/sys/kbd/c", func(ui.Event) {
 		if exitGauge.Display {
 			exitGauge.Display = false
-			ui.Body.Rows = ui.Body.Rows[1:]
+			exitGauge.Height = 0
+			exitGauge.Width = 0
+			exitGauge.Border = false
 			ui.Body.Align()
 			ui.Clear()
 			ui.Render(ui.Body)
@@ -161,28 +214,92 @@ func (c *JobWatchCommand) addExitAfterGauge() {
 	})
 }
 
-func (c *JobWatchCommand) jobStatus() (ui.GridBufferer, bool, int) {
-	// Try querying the job
-	jobID := c.config.JobID
-	jobs, _, err := c.client.Jobs().PrefixList(jobID)
+func (c *JobWatchCommand) pollJob(job *api.Job) {
+	q := &api.QueryOptions{
+		WaitIndex: job.ModifyIndex,
+	}
+	for {
+		updated, _, err := c.client.Jobs().Info(job.ID, q)
+		if err != nil {
+			if strings.Contains(err.Error(), "job not found") {
+				ui.SendCustomEvt("/job/stopped", fmt.Errorf("Failed to poll job: %v", err))
+			} else {
+				ui.SendCustomEvt("/watch/job", fmt.Errorf("Failed to poll job: %v", err))
+			}
+			return
+		}
+
+		if updated != nil {
+			ui.SendCustomEvt("/watch/job", updated)
+			q.WaitIndex = updated.ModifyIndex
+		}
+	}
+}
+
+type JobStatusGrid struct {
+	*ui.Par
+	job *api.Job
+}
+
+func NewJobStatusGrid(job *api.Job) *JobStatusGrid {
+	js := &JobStatusGrid{
+		job: job,
+		Par: ui.NewPar(""),
+	}
+	js.Height = 8
+	js.Border = true
+	js.BorderLabel = "Job Info"
+	js.render()
+
+	// Handle updates
+	js.Handle("/watch/job", func(e ui.Event) {
+		job, ok := e.Data.(*api.Job)
+		if !ok {
+			panic(e.Data)
+		}
+
+		js.job = job
+		js.render()
+		ui.Body.Align()
+		ui.Clear()
+		ui.Render(ui.Body)
+	})
+
+	return js
+}
+
+func (js *JobStatusGrid) Update(job *api.Job) {
+	js.job = job
+	js.render()
+}
+
+func (js *JobStatusGrid) render() {
+	// Check if it is periodic
+	sJob, err := convertApiJob(js.job)
 	if err != nil {
-		c.Ui.Error(fmt.Sprintf("Error querying job: %s", err))
-		return nil, true, 1
+		ui.SendCustomEvt("/watch/job", fmt.Errorf("Failed to poll job: %v", err))
+		return
 	}
-	if len(jobs) == 0 {
-		c.Ui.Error(fmt.Sprintf("No job(s) with prefix or id %q found", jobID))
-		return nil, true, 1
-	}
-	if len(jobs) > 1 && strings.TrimSpace(jobID) != jobs[0].ID {
-		c.Ui.Output(fmt.Sprintf("Prefix matched multiple jobs\n\n%s", createStatusListOutput(jobs)))
-		return nil, true, 0
-	}
-	// Prefix lookup matched a single job
-	job, _, err := c.client.Jobs().Info(jobs[0].ID, nil)
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf("Error querying job: %s", err))
-		return nil, true, 1
+	periodic := sJob.IsPeriodic()
+
+	// Format the job info
+	basic := []string{
+		fmt.Sprintf("ID|%s", js.job.ID),
+		fmt.Sprintf("Name|%s", js.job.Name),
+		fmt.Sprintf("Type|%s", js.job.Type),
+		fmt.Sprintf("Priority|%d", js.job.Priority),
+		fmt.Sprintf("Datacenters|%s", strings.Join(js.job.Datacenters, ",")),
+		fmt.Sprintf("Status|%s", js.job.Status),
+		fmt.Sprintf("Periodic|%v", periodic),
 	}
 
-	return nil, false, 0
+	if periodic {
+		now := time.Now().UTC()
+		next := sJob.Periodic.Next(now)
+		basic = append(basic, fmt.Sprintf("Next Periodic Launch|%s",
+			fmt.Sprintf("%s (%s from now)",
+				formatTime(next), formatTimeDifference(now, next, time.Second))))
+	}
+
+	js.Text = formatKV(basic)
 }
